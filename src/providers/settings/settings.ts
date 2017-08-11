c7e615e7d5dbebefdd6538c4029feb11d27c38e6
zhouzhouthezhou <kylezhou2222@gmail.com>
2017-08-10 23:06:20 -0600

    last commit before i pack up to go home, and yes ive been programming while on vacation. this is why i cant have nice things (like a life)

--- src/providers/settings/settings.ts
+++ src/providers/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 /*
@@ -7,12 +8,14 @@ import 'rxjs/add/operator/map';
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular DI.
-*/
-@Injectable()
-export class SettingsProvider {
+  	*/
+  @Injectable()
+  export class SettingsProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello SettingsProvider Provider');
+  	private theme: BehaviorSubject<String>;
+
+  	constructor(public http: Http) {
+  		this.theme = new BehaviorSubject('light-theme');
+  	}
+
   }
-
-}
